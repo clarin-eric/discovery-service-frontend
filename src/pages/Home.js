2a0ce2d1b527dd338f6a770e4edadf3aab74b31e
Willem Elbers <wjm.elbers@gmail.com>
2018-02-05 16:51:12 +0100

    Added images for flags

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import {Row, Col} from 'react-bootstrap';
+import './Home.css';
+import FLAG_EU from './flags/europeanunion.png';
+import FLAG_DK from './flags/dk.png';
 
 class Home extends Component {
     render() {
         return (
             <div className="container">
                 <Row>
-                    <Col lg={4} lgOffset={4}>
-                        <div>
-                            CLARIN eu website account
-                        </div>
+                    <Col lg={4} lgOffset={4} className="idp">
+                        <div className="idp-title">CLARIN eu website account</div>
+                        <div className="idp-country"><img src={FLAG_EU} alt="Flag EU"></img>European union</div>
                     </Col>
                 </Row>
                 <Row>
-                    <Col lg={4} lgOffset={4}>
-                        <div>
-                            Aalborg university
-                        </div>
+                    <Col lg={4} lgOffset={4} className="idp">
+                        <div className="idp-title">Aalborg university</div>
+                        <div className="idp-country"><img src={FLAG_DK} alt="Flag DK"></img> Denmark</div>
                     </Col>
                 </Row>
             </div>
