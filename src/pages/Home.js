6805e66d1cd7410a67d6f6bc6e92d2bf52adccf1
Willem Elbers <wjm.elbers@gmail.com>
2018-02-05 19:30:56 +0100

    Implemented properties for idp component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import {Row, Col} from 'react-bootstrap';
 import './Home.css';
-import FLAG_EU from './flags/europeanunion.png';
-import FLAG_DK from './flags/dk.png';
+import Idp from './Idp.js';
 
 class Home extends Component {
     render() {
         return (
             <div className="container">
                 <Row>
-                    <Col lg={4} lgOffset={4} className="idp">
-                        <div className="idp-title">CLARIN eu website account</div>
-                        <div className="idp-country"><img src={FLAG_EU} alt="Flag EU"></img>European union</div>
+                    <Col lg={4} lgOffset={4} >
+                       <Idp name={"CLARIN.eu website account"} country={"eu"} logo={"https://www.clarin.eu/sites/default/files/clarin-logo.png"} />
                     </Col>
                 </Row>
                 <Row>
-                    <Col lg={4} lgOffset={4} className="idp">
-                        <div className="idp-title">Aalborg university</div>
-                        <div className="idp-country"><img src={FLAG_DK} alt="Flag DK"></img> Denmark</div>
+                    <Col lg={4} lgOffset={4} >
+                        <Idp name={"Aalborg university"} country={"dk"} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={4} lgOffset={4} >
+                        <Idp name={"Academy of Arts, Architecture and Design in Prague"} country={"cz"} logo={"https://www.umprum.cz/logos/um2-400-cz.png"} />
                     </Col>
                 </Row>
             </div>
