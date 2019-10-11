3d926724fb11192f535f2cf19ebaff8bd5eb837c
Willem Elbers <wjm.elbers@gmail.com>
2019-10-11 08:53:32 +0200

    Change into final directory and list contents

--- travis_release.sh
+++ travis_release.sh
@@ -10,4 +10,5 @@ if [ -z ${VERSION+x} ]; then
 else
     sed -i "s/{{VERSION}}/${VERSION}/g" build/config.js
     cd build && tar -pczf "../discovery-service-frontend-${VERSION}.tar.gz" *
+    cd ..&& pwd && ls -l
 fi
